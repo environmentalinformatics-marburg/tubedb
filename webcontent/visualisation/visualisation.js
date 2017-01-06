c4633b2845c15ba782207bbf30f7a9727b6d9ac9
swoellauer <swoellauer>
2017-01-06 15:53:23 +0100

    interactive visualisation with multi select plot/sensor, timeframe, quality, type and css style

--- webcontent/visualisation/visualisation.js
+++ webcontent/visualisation/visualisation.js
@@ -7,6 +7,7 @@ var url_generalstation_list = url_api_base + "tsdb/generalstation_list";
 var url_plot_list = url_api_base + "tsdb/plot_list";
 var url_sensor_list = url_api_base + "tsdb/sensor_list";
 var url_query_image = url_api_base + "tsdb/query_image";
+var url_query_heatmap = url_api_base + "tsdb/query_heatmap";
 
 function init() {
 	
@@ -16,8 +17,8 @@ Vue.component('visualisation-interface', {
 template: '#visualisation-template',
 
 props: {
-	projectID: String,
-},
+	project: Object,
+}, //end props
 
 data: function () {
 	return {
@@ -27,14 +28,37 @@ data: function () {
 		groupID: "*",
 		plotHover: false,
 		plots: [],
-		plotID: "",
+		plotIDs: [],
 		sensorHover: false,
 		sensors: [],
 		sensorIDs: ["*"],
 		sensorMap: {},
+		timeframeHover: false,
+		timeframeYear: "*",
+		timeframeMonths: ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"],
+		timeframeMonthsNumber: {"*":0, "jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10 ,"nov":11, "dec":12},
+		timeframeMonth: "*",
+		aggregations: ["raw","hour","day","week","month","year"],
+		aggregationsText: {raw:"raw data", hour:"hours", day:"days", week:"weeks", month:"months", year:"years"},
+		aggregation: "hour",
+		qualityHover: false,
+		qualities: ["no", "physical", "step", "empirical"],
+		qualitiesText: {no:"0: no", physical:"1: physical", step:"2: physical,step", empirical:"3: physical,step,empirical"},
+		quality: "physical",
+		interpolated: false,
+		settingsHover: false,
+		viewTypes: ["diagram", "heatmap", "boxplot"],
+		viewType: "diagram",
+		widthText: "auto",
+		widthCustom: 1000,
+		heightTexts: ["small", "normal", "large", "custom"],
+		heightTextsMap: {small:100, normal:400, large:800},
+		heightText: "small",
+		heightCustom: 100,
 		views: [],
+		max_display_views: 500,
 	}
-},
+}, //end data
 
 computed: {
 	groupMap: function() {
@@ -44,17 +68,37 @@ computed: {
 		})
 		return values;
 	},
+	timeframeYears: function() {
+		var start = this.project.view_year_range.start;
+		var end = this.project.view_year_range.end;
+		var y = [];
+		for(var i=start; i<=end; i++) {
+			y.push(i);
+		}
+		return y;
+	},
+	display_views: function() {	
+		return this.views.length <= this.max_display_views ? this.views : this.views.slice(0, this.max_display_views);
+	},
+	viewHeight: function(){
+		if(this.heightText=="custom") {
+			var v = this.heightCustom;
+			return this.isValidSize(v) ? v : 100;
+		} else {
+			return this.heightTextsMap[this.heightText];			
+		}
+	},
 },
 
 mounted: function () {
 	this.updateGroups();
-},
+}, //end mounted
 
 methods: {
 	updateGroups: function() {
 		var self = this;
 		self.appMessage = "query groups...";	
-		axios.get(url_generalstation_list, {params: {region: self.projectID}})
+		axios.get(url_generalstation_list, {params: {region: self.project.id}})
 		.then(function(response) {
 			self.groups = response.data.split('\n').map(function(row) {
 				var cols = row.split(';');
@@ -74,14 +118,14 @@ methods: {
 	updatePlots: function() {
 		var self = this;
 		self.appMessage = "query plots...";
-		var params = self.groupID=="*" ? {region: self.projectID} : {generalstation: self.groupID}; 
+		var params = self.groupID=="*" ? {region: self.project.id} : {generalstation: self.groupID}; 
 		axios.get(url_plot_list, {params: params})
 		.then(function(response) {
 			self.plots = response.data.split('\n').map(function(row) {
 				var cols = row.split(';');
 				return {id: cols[0], a: cols[1], b:cols[2]};
 			});
-			self.plotID = self.plots[0].id;
+			self.plotIDs = [self.plots[0].id];
 			self.appMessage = null;
 		})
 		.catch(function(error) {
@@ -91,18 +135,41 @@ methods: {
 	updateSensors: function() {
 		var self = this;
 		self.appMessage = "query sensors...";
-		var params = {plot: this.plotID}; 
+		var params;
+		if(this.plotIDs[0] == '*' || this.plotIDs.length>1 ) {
+			params = self.groupID=="*" ? {region: self.project.id} : {general_station: self.groupID};
+		} else {
+			params = {plot: self.plotIDs[0]}; 
+		}
 		axios.get(url_sensor_list, {params: params})
 		.then(function(response) {
+			var prevIDs = self.sensorIDs;
 			self.sensors = response.data.split('\n').map(function(row) {
 				var cols = row.split(';');
 				return {id: cols[0], a: cols[1], b:cols[2]};
 			});
-			if(self.sensorIDs[0] == ["*"]) {
-				self.updateViews();
+			
+			var newIDs = [];
+			if(prevIDs[0]=='*') {
+				newIDs = ['*'];
 			} else {
-				self.sensorIDs = ["*"];
+				newIDs = prevIDs.filter(function(id) {return self.sensors.find(function(s) {return s.id==id;}) != undefined;});
 			}
+			
+			console.log(newIDs);
+			
+			if(newIDs.length == 0) {
+				newIDs.push('*');
+			}
+			
+			self.sensorIDs = newIDs;
+			
+			if(JSON.stringify(prevIDs) === JSON.stringify(newIDs)) {
+				self.updateViews();
+			}
+
+			
+			console.log("prev "+prevIDs+"  now "+self.sensorIDs);
 			var values = {};
 			self.sensors.forEach(function(o){
 				values[o.id] = o;
@@ -115,37 +182,131 @@ methods: {
 		});
 	},
 	getDiagramURL: function(view) {
-		var query = "plot="+view.plot+"&"+"sensor="+view.sensor+"&"+"width="+view.width+"&"+"height="+view.height;
+		var query = "plot="+view.plot+"&"+"sensor="+view.sensor+"&"+"width="+view.width+"&"+"height="+view.height+"&"+"aggregation="+view.aggregation+"&"+"quality="+view.quality+"&"+"interpolated="+view.interpolated+"&"+"boxplot="+view.boxplot;
+		if(view.year!='*') {
+			query += '&year='+view.year;
+			if(view.month>0) {
+				query += '&month='+view.month;
+			}
+		}
 		return url_query_image+"?"+query;
 	},
+	getHeatmapURL: function(view) {
+		var query = "plot="+view.plot+"&"+"sensor="+view.sensor+"&"+"quality="+view.quality+"&"+"interpolated="+view.interpolated;
+		if(view.year!='*') {
+			query += '&year='+view.year;
+			if(view.month>0) {
+				query += '&month='+view.month;
+			}
+		}
+		return url_query_heatmap+"?"+query;
+	},
 	updateViews: function() {
+		
+		var flatMap = function(array, callback) { // source: http://obscurejavascript.tumblr.com/post/144412398726/flatmap-in-javascript
+			return [].concat.apply([], array.map(callback));
+		};
+		
 		var self = this;
 		var container = document.getElementById("container");
-		var width = container.clientWidth - 30 < 100? 100 : container.clientWidth - 30;
+		var width = this.widthText=="auto" ? (container.clientWidth - 30 < 100? 100 : container.clientWidth - 30) : (this.isValidSize(this.widthCustom) ? this.widthCustom : 1000);
 		var height = 100; 
 		var sensors = this.sensorIDs[0]=="*" ? this.sensors : this.sensorIDs.map(function(sensorID){return self.sensorMap[sensorID]});
-		this.views = sensors.map(function(sensor) {
-			return {type: "diagram", plot:self.plotID, sensor:sensor.id, width:width, height:height}; 
+		/*this.views = sensors.map(function(sensor) {
+			return {type: "diagram", 
+			        plot: self.plotIDs[0], 
+					sensor: sensor.id, 
+					width: width, 
+					height: height, 
+					year: self.timeframeYear, 
+					month: self.timeframeMonthsNumber[self.timeframeMonth], 
+					aggregation: self.aggregation,
+					quality: self.quality,
+					interpolated: self.interpolated}; 
+		});*/
+		
+		/*this.views = flatMap(self.plotIDs, function(plotID) {
+			return sensors.map(function(sensor) {
+				return {type: "diagram", 
+						plot: self.plotIDs[0], 
+						sensor: sensor.id, 
+						width: width, 
+						height: height, 
+						year: self.timeframeYear, 
+						month: self.timeframeMonthsNumber[self.timeframeMonth], 
+						aggregation: self.aggregation,
+						quality: self.quality,
+						interpolated: self.interpolated}; 
+			});
+		});*/
+		
+		var plotIDs = self.plotIDs[0]=='*' ? self.plots.map(function(p) {return p.id;}) : self.plotIDs;
+		
+		this.views = flatMap(sensors, function(sensor) {
+			return plotIDs.map(function(plotID) {
+				return {type: self.viewType, 
+						plot: plotID, 
+						sensor: sensor.id, 
+						width: width, 
+						height: self.viewHeight, 
+						year: self.timeframeYear, 
+						month: self.timeframeMonthsNumber[self.timeframeMonth], 
+						aggregation: self.aggregation,
+						quality: self.quality,
+						interpolated: self.interpolated,
+						boxplot: self.viewType=="boxplot"}; 
+			});
 		});
 	},
-},
+	
+	isValidSize: function(v) {
+		return v>=10 && v<=4096;
+	},
+}, //end methods
 
 watch: {
-	projectID: function () {
+	project: function () {
 		this.updateGroups();
 	},
 	groupID: function() {
 		this.groupHover = false;
 		this.updatePlots();
 	},
-	plotID: function() {
+	plotIDs: function() {
 		this.plotHover = false;
 		this.updateSensors();
 		
 	},
 	sensorIDs: function() {
 		this.updateViews();
-	}
+	},
+	timeframeYear: function() {
+		this.updateViews();
+	},
+	timeframeMonth: function() {
+		this.updateViews();
+	},
+	aggregation: function() {
+		this.updateViews();
+	},
+	quality: function() {
+		this.updateViews();
+	},
+	interpolated: function() {
+		this.updateViews();
+	},
+	viewType: function() {
+		this.updateViews();
+	},
+	viewWidth: function() {
+		this.updateViews();
+	},
+	widthCustom: function() {
+		this.updateViews();
+	},
+	viewHeight: function() {
+		this.updateViews();
+	},
 }, //end watch
 
 });	//end visualisation-interface
@@ -162,7 +323,7 @@ data: {
 	projects: [],
 	projectMap: {},
 	projectID: "",
-},
+}, //end data
 
 mounted: function () {
 	var self = this;
