3b1e8f0c863fa7a04aad9d6deda54c7392950a06
swoellauer <swoellauer>
2018-04-04 09:13:35 +0200

    be config replaced on station calculated sensors with virtual sensors, removed import of replaced sensors

--- src/tsdb/TsDB.java
+++ src/tsdb/TsDB.java
@@ -33,7 +33,7 @@ import tsdb.util.Util;
 public class TsDB implements AutoCloseable {
 	private static final Logger log = LogManager.getLogger();
 	
-	public static final String tubedb_version = "1.8.5";
+	public static final String tubedb_version = "1.8.6";
 
 	/**
 	 * map regionName -> Region
