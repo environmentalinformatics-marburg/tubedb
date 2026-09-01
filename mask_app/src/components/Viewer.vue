7deec8dd78cb9810d25c3f56a0b356442dc8549d
swoellauer <swoellauer@users.noreply.github.com>
2026-09-01 09:22:11 +0200

    mask_app diagram view move and zoom

--- mask_app/src/components/Viewer.vue
+++ mask_app/src/components/Viewer.vue
@@ -23,10 +23,122 @@ const props = defineProps({
     required: true
   },
   data: {
-    type: Array,
+    type: [Array, null],
     required: true,
   }
-})
+});
+
+
+function wheelZoomPlugin(opts) {
+  let factor = opts.factor || 0.75;
+
+  let xMin, xMax, xRange;
+
+  function clamp(nRange, nMin, nMax, fRange, fMin, fMax) {
+    if (nRange > fRange) {
+      nMin = fMin;
+      nMax = fMax;
+    }
+    else if (nMin < fMin) {
+      nMin = fMin;
+      nMax = fMin + nRange;
+    }
+    else if (nMax > fMax) {
+      nMax = fMax;
+      nMin = fMax - nRange;
+    }
+
+    return [nMin, nMax];
+  }
+
+  return {
+    hooks: {
+      ready: u => {
+        xMin = u.scales.x.min;
+        xMax = u.scales.x.max;
+        xRange = xMax - xMin;
+
+        let over = u.over;
+        let rect = over.getBoundingClientRect();
+
+        over.addEventListener("wheel", e => {
+          e.preventDefault();
+
+          let {left} = u.cursor;
+          let leftPct = left / rect.width;
+          
+          let xVal = u.posToVal(left, "x");
+          let oxRange = u.scales.x.max - u.scales.x.min;
+
+          let nxRange = e.deltaY < 0 ? oxRange * factor : oxRange / factor;
+          let nxMin = xVal - leftPct * nxRange;
+          let nxMax = nxMin + nxRange;
+          
+          [nxMin, nxMax] = clamp(nxRange, nxMin, nxMax, xRange, xMin, xMax);
+
+          u.batch(() => {
+            u.setScale("x", {
+              min: nxMin,
+              max: nxMax,
+            });
+          });
+        });
+      }
+    }
+  };
+}
+
+function dragPlugin(opts) {
+
+  let mouseDownXval;
+  let mouseDownXmin;
+  let mouseDownXmax;
+
+  function ready(u) {
+    let plot = u.root.querySelector(".u-over");
+
+    function initMove() {
+      let xVal = u.posToVal(u.cursor.left, "x");
+      mouseDownXval = xVal;
+      mouseDownXmin = u.scales.x.min;
+      mouseDownXmax = u.scales.x.max;
+    }
+
+    function finishMove() {
+      mouseDownXval = undefined;
+    }
+
+    plot.addEventListener("mousedown", function(e) {
+      initMove();
+    });
+
+    plot.addEventListener("mouseup", function(e) {
+      finishMove();
+    });
+
+    plot.addEventListener("mousemove", function(e) {
+      if(e.buttons === 1) {
+        if(mouseDownXval === undefined) {
+          initMove();
+        } else {
+          let xVal = u.posToVal(u.cursor.left, "x");
+          let xDelta = mouseDownXval - xVal;
+          let nxMin = mouseDownXmin + xDelta;
+          let nxMax = mouseDownXmax + xDelta
+          u.batch(() => {
+            u.setScale("x", {
+              min: nxMin,
+              max: nxMax,
+            });
+          });
+          mouseDownXmin = nxMin;
+          mouseDownXmax = nxMax;
+        }
+      }
+    });
+  }
+  return {hooks: {ready}};
+}
 
 const options = computed(() => ({
   width: props.width,
@@ -39,6 +151,10 @@ const options = computed(() => ({
       y: false,
     },
   },
+  plugins: [
+    wheelZoomPlugin({factor: 0.75}),
+    dragPlugin({})
+  ],
   series: [
     {},
     {
